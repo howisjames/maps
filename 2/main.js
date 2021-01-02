a40bf482021f6b606a3f197de86eff2e5f664f5d
James C Taylor <35105197+howisjames@users.noreply.github.com>
2021-01-01 20:26:49 -0600

    Update main.js
    zoom out initial view

--- 2/main.js
+++ 2/main.js
@@ -24,7 +24,7 @@ let panelID = "my-info-panel";
  */
 function init() {
   // Create a new Leaflet map centered on the state of Texas
-  map = L.map("map").setView([30.7367, -98.37], 12); 	
+  map = L.map("map").setView([30.7367, -98.37], 8); 	
 
   // This is the Carto Positron basemap
   L.tileLayer(
